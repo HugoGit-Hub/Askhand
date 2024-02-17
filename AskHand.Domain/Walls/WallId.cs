e16a860ce3e089848058fa70ed2369e4678875b0
Hugo DECUQ <hugo.decuq@genapi.fr>
2024-02-17 10:04:47 +0100

    Create WallId value object with tests and exceptions

---
+++ AskHand.Domain/Walls/WallId.cs
@@ -0,0 +1,21 @@
+﻿namespace AskHand.Domain.Walls;
+
+public sealed class WallId : ValueObject
+{
+    public Guid Value { get; }
+
+    private WallId (Guid value)
+    {
+        Value = value;
+    }
+
+    public static WallId CreateUnique()
+    {
+        return new WallId(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
