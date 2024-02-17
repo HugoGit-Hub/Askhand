56f3c378beb4dcb9577cb8dcef194e0352bb42ce
Hugo DECUQ <hugo.decuq@genapi.fr>
2024-02-17 10:03:33 +0100

    Create Country value object with tests and exceptions

---
+++ AskHand.Domain/Countries/Exceptions/CountryMissingArgumentException.cs
@@ -0,0 +1,9 @@
+﻿namespace AskHand.Domain.Countries.Exceptions;
+
+public class CountryMissingArgumentException : Exception
+{
+    public CountryMissingArgumentException(string message)
+        : base(message)
+    {
+    }
+}
