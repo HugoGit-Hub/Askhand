fa7ad11c633f1066b3fed4f4e2313e75aede760d
hugo <hugo.decuq.34@icloud.com>
2024-02-29 12:58:45 +0100

    Use Infrastructure DI into middle ware pipeline

--- AskHand.Api/Program.cs
+++ AskHand.Api/Program.cs
@@ -1,8 +1,12 @@
+using AskHand.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
