4d5f50d095058ab5b31152425bb06b969542c1cf
hugo <hugo.decuq.34@icloud.com>
2024-02-29 12:59:20 +0100

    Create and test Create method of ClimbId Value object

--- AskHand.Test/Domain/Climbs/ClimbIdTest.cs
+++ AskHand.Test/Domain/Climbs/ClimbIdTest.cs
@@ -41,4 +41,19 @@ public class ClimbIdTest
         // Assert
         Assert.False(result);
     }
+
+    [Fact]
+    public void Create_ShouldReturnClimbId_WithSameGuidValue()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+
+        // Act
+        var climbId = ClimbId.Create(guid);
+
+        // Assert
+        Assert.NotNull(climbId);
+        Assert.IsType<ClimbId>(climbId);
+        Assert.Equal(guid, climbId.Value);
+    }
 }
