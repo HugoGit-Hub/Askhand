abe462677c80dd2cba8f96eb7e7c5d7c6cd5af37
hugo <hugo.decuq.34@icloud.com>
2024-03-04 10:26:36 +0100

    Create FriendshipConfiguration

---
+++ src/AskHand.Infrastructure/Context/Configurations/FriendshipConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using AskHand.Domain.Friendships;
+using AskHand.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AskHand.Infrastructure.Context.Configurations;
+
+public class FriendshipConfiguration : IEntityTypeConfiguration<Friendship>
+{
+    public void Configure(EntityTypeBuilder<Friendship> builder)
+    {
+        ConfigureFriendshipTable(builder);
+    }
+
+    private static void ConfigureFriendshipTable(EntityTypeBuilder<Friendship> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id)
+            .ValueGeneratedNever()
+            .HasConversion(
+                id => id.Value,
+                value => FriendshipId.Create(value));
+
+        builder.Property(e => e.UserIdOne)
+            .ValueGeneratedNever()
+            .HasConversion(
+                userIdOne => userIdOne.Value,
+                value => UserId.Create(value));
+        
+        builder.Property(e => e.UserIdTwo)
+            .ValueGeneratedNever()
+            .HasConversion(
+                userIdOne => userIdOne.Value,
+                value => UserId.Create(value));
+
+        builder.Property(e => e.StartFriendshipDate);
+        builder.Property(e => e.EndFriendshipDate);
+    }
+}
